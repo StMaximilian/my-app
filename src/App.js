d8566f9db1a683658171e103cda86e10cdef964e
GaponMV <tax99@mail.ru>
2021-08-16 12:05:52 +0700

    Попытка в навигацию

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, {useEffect} from "react";
 import TodoList from "./ToDo/TodoList";
 import Context from "./context";
 import Loader from "./Loader";
-import Modal from "./Model/Model";
+import { Link, Switch, Route } from 'react-router';
+import Nav from './Navbar'
+import Page1 from './page1';
+import Page2 from './page2';
+import Page3 from './page3';
 
 const AddTodo = React.lazy(() => import('./ToDo/AddTodo'))
 
-function App() {
+ function App() {
   const [todos,setTodos] = React.useState([])
   const [load, setLoad] = React.useState(true)
 
   useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=15')
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
   .then(response => response.json())
   .then(todos =>setTimeout(() => {
     setTodos(todos)
@@ -42,21 +46,31 @@ function App() {
   }
 
   return (
-   <Context.Provider value={{removeTodo}}>
-     <div className="wrapper">
-        <h1>Hello world!</h1>
-        <Modal></Modal>
+   <Context.Provider value={{removeTodo}}> 
+   <Router>
+   <Nav />
+              <Switch>
+                <Route exactly component={Landing} pattern="/" />
+                <Route exactly component={Page1} pattern="/path1" />
+                <Route exactly component={Page2} pattern="/path2" />
+                <Route exactly component={Page3} pattern="/path3" />
+                <Route component={Page404} />
+              </Switch>
+     <div className='wrapper'> 
         <React.Suspense fallback={<p>Loading......</p>}>
-           <AddTodo onCreate={addTodo}/>
+        <AddTodo onCreate={addTodo}/>
         </React.Suspense>     
-        {load && <Loader/>}
-        {todos.length ?  (<TodoList todos={todos} onTog={toggleTodo}/>) :
-        load ? null :(<p>No tasks!</p>)}
-             
-      </div>
-   </Context.Provider>
-        
+            {load && <Loader/>}
+            {todos.length ?  (<TodoList todos={todos} onTog={toggleTodo}/>) :
+            load ? null :(<p>No tasks!</p>)} 
+        </div>
+      </Router>
+   </Context.Provider>    
   )
 }
 
-export default App;
+export default App
+
+
+
+
