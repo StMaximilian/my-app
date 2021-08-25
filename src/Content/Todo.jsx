0e14a441b67ffacf65c93a9bd7664bce64454562
GaponMV <tax99@mail.ru>
2021-08-25 12:54:08 +0700

    Ручной наблюдатей, авторизации нет

--- src/Content/Todo.jsx
+++ src/Content/Todo.jsx
@@ -1,55 +1,91 @@
 import { observer } from "mobx-react-lite";
 import ToDo from "../Store/ToDo";
 import "../index.css";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 const Todo = observer(() => {
   return (
     <div>
-      {ToDo.todos.map((t) => (
-        <div>
-          Tasks left: {t.unfinishedTodoCount}
-          <ul>
-            <li>
-              <span>
-                <input
-                  className="itemsinput"
-                  type="checkbox"
-                  checked={t.completed}
-                  onChange={() => ToDo.completeTodo(t.id)}
-                />
-                <strong>{t.id + "."}</strong>
-                &nbsp;
-                {t.title + "  " + t.daten}
-                &nbsp;
-              </span>
-              <button onClick={() => ToDo.removeTodo(t.id)}>&times;</button>
-            </li>
-          </ul>      
+      <form className="grocery-form" onSubmit={ToDo.AddEditTodo}>
+        <h3>Todo List</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Вставьте заметку"
+            value={ToDo.inname}
+            onChange={(e) => (ToDo.inname = e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {ToDo.isEdit ? "Изменить" : "Внести"}
+          </button>
         </div>
-      ))}
+      </form>
+      <ul>
+        {ToDo.todos.map((todo, index) => (
+          // <TodoItem key={todo.id}></TodoItem>
+          <div key={todo.id}>
+            <span className="">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => ToDo.completeTodo(todo.id)}
+              />
+              <strong>{index + 1 + "."}</strong>
+              &nbsp;
+              {todo.title}
+              &nbsp;
+              <button
+                type="button"
+                className="edit-btn"
+                onClick={() => ToDo.editTodo(todo.id)}
+              >
+                <FaEdit />
+              </button>
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => ToDo.removeTodo(todo.id)}
+              >
+                <FaTrash />
+              </button>
+            </span>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 });
 
 export default Todo;
 
-{
-  /* /* {ToDo.todos.map((t) => (
-          <li>
-            <span>
-              <input
-                className="itemsinput"
-                type="checkbox"
-                checked={t.completed}
-                onChange={() => ToDo.completeTodo(t.id)}
-              />
-              <strong>{t.id + "."}</strong>
-              &nbsp;
-              {t.title}
-              &nbsp;
-              {t.date}
-              &nbsp;
-            </span>
-            <button onClick={() => ToDo.removeTodo(t.id)}>&times;</button>
-          </li>)}         */
+function TodoItem(todo) {
+  return (
+    <div>
+      <span className="">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => ToDo.completeTodo(todo.id)}
+        />
+        {/* <strong>{index + 1 + "."}</strong> */}
+        &nbsp;
+        {todo.title}
+        &nbsp;
+        <button
+          type="button"
+          className="edit-btn"
+          onClick={() => ToDo.editTodo(todo.id)}
+        >
+          <FaEdit />
+        </button>
+        <button
+          type="button"
+          className="delete-btn"
+          onClick={() => ToDo.removeTodo(todo.id)}
+        >
+          <FaTrash />
+        </button>
+      </span>
+    </div>
+  );
 }
--- src/Store/ToDo.js
+++ src/Store/ToDo.js
@@ -1,8 +1,19 @@
-import { makeObservable, observable, computed, action } from "mobx";
+import {
+  makeObservable,
+  makeAutoObservable,
+  observable,
+  computed,
+  action,
+} from "mobx";
 
 class Todo {
   flag = false;
-  userid;
+  userid = null;
+  editId = null;
+  isEdit = false;
+  inname = '';
+  count = 10;
+  // todos=[]
 
   todos = JSON.parse(localStorage.getItem("notes"));
   //   todos =  JSON.parse(localStorage.getItem("notes")).filter(
@@ -15,23 +26,65 @@ class Todo {
 
   constructor(todos) {
     makeObservable(this, {
+      flag: observable,
+      userid: observable,
+      editId: observable,
+      isEdit: observable,
+      inname: observable,
       todos: observable,
-      addTodo: action,
+      AddEditTodo: action,
       removeTodo: action,
+      editTodo: action,
       completeTodo: action,
       unfinishedTodoCount: computed,
+      count: observable,
     });
   }
 
-  addTodo(todoObj) {
-    this.todos.push(todoObj);
-    localStorage.setItem("notes", JSON.stringify(this.todos));
+  // constructor(todos) {
+  //   makeAutoObservable(this);
+  // }
 
+  AddEditTodo(e) {
+    e.preventDefault();
+    if (this.inname === '') {
+      alert("Вы вводите пустое поле");
+    } else if (this.inname && this.isEdit) {
+      this.todos = this.todos.map((item) => {
+        if (item.id === this.editID) {
+          return { ...item, title: this.inname };
+        }
+        return item;
+      });
+      this.inname = "";
+      this.editID = null;
+      this.isEdit = false;
+      alert("Изменение произошло");
+      localStorage.setItem("notes", JSON.stringify(this.todos));
+    } else {
+      alert("Заметка добавлена");
+      this.count++;
+      const newItem = {
+        id: this.count,
+        title: this.inname,
+        date: new Date().getTime().toString(),
+      };
+      this.todos.push(newItem);
+      this.inname = "";
+      localStorage.setItem("notes", JSON.stringify(this.todos));
+    }
   }
 
   removeTodo(id) {
     this.todos = this.todos.filter((v) => v.id !== id);
     localStorage.setItem("notes", JSON.stringify(this.todos));
+  }
+
+  editTodo(id) {
+    const specificItem = this.todos.find((item) => item.id === id);
+    this.isEdit = true;
+    this.editId = id;
+    this.bufname = specificItem.title;
   }
 
   completeTodo(id) {
@@ -42,7 +95,6 @@ class Todo {
       return todo;
     });
     localStorage.setItem("notes", JSON.stringify(this.todos));
-
   }
 }
 
